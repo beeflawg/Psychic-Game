f1b902e8b12f19edac81ce779f43bae54645533b
beef <beeflawg@gmail.com>
2018-10-04 13:42:44 -0700

    Final Files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Psychic-Game
+Classic game of tyring to guess the letter the computer had picked.
+This project demonstrates the use of functions, vars, and if statements.
---
+++ assets/Javascript/game.js
@@ -0,0 +1,54 @@
+function genAlphabet() {
+    var alph = [];
+
+    for (var i = 65; i < 91; i++) {
+        alph.push(String.fromCharCode(i).toLowerCase());
+    }
+
+    return alph;
+}
+
+var alphabet = genAlphabet();
+var computerChoices = alphabet;
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var letters = [];
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+    guesses--;
+
+    if (userGuess === computerGuess) {
+        wins = wins + 1;
+        document.getElementById("userWins").innerHTML = "Wins: " + wins;
+        guesses = 9
+        document.getElementById("guessesLeft").innerHTML = "Guesses left: " + guesses;
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    }
+
+    else if (guesses < 9) {
+        letters = letters + userGuess;
+        document.getElementById("guessesLeft").innerHTML = "Guesses left: " + guesses;
+        document.getElementById("lettersGuessed").innerHTML = "Your Gueeses So Far: " + letters;
+
+    }
+
+    if (guesses === 0) {
+        losses++;
+        document.getElementById("userLosses").innerHTML = "Losses: " + losses;
+        guesses = 9
+        document.getElementById("guessesLeft").innerHTML = "Guesses left: " + guesses;
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    }
+
+    console.log(computerGuess)
+    console.log(losses)
+}
